be0d1c4060e9a8f458b26b78a7a5790caf165e1b
Castillo666 <43051849+Castillo666@users.noreply.github.com>
2019-03-27 08:22:46 -0600

    Saludos

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import com.sun.webkit.ContextMenu;
+import javax.swing.JOptionPane;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showConfirmDialog(null, "Meperdonas");
+    }
+    
+}
